ee153864f7e0f932fd193f63c50d1be8ceb172c2
oguimbal <olivier.guimbal@gadz.org>
2019-08-26 10:52:12 +0200

    Fix types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
 
 export type Partial<T> = { [P in keyof T]?: T[P]; };
-export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]>; };
+export type DeepPartial<T> = {
+    // deeppartial with workaround for bug https://github.com/microsoft/TypeScript/issues/21592#issuecomment-496723647
+    [P in keyof T]?: T[P] extends never ? DeepPartial<T[P]> : DeepPartial<T[P]>
+};
 export const nameof = <T>(name: keyof T) => name;
 export interface Ctor<T> extends Function {
     new(...params: any[]): T; prototype: T;
 }
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
